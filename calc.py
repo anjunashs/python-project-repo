d144ccd4b5c10bb23dbfc9b73d5d81ec86ce9e60
anjunashs <117129615+anjunashs@users.noreply.github.com>
2022-10-31 22:25:38 -0400

    calculator programme added

---
+++ calc.py
@@ -0,0 +1,24 @@
+#calculates addition subration multiplication and divison
+
+#assigning variables
+print('enter value for a')
+a=input()
+print('enter value for b')
+b=input()
+
+#addition
+print('addition of a and b is:')
+print(a+b)
+
+#subtraction
+print('subtraction of a and b is:')
+print(a-b)
+
+#multiplication
+print('multiplication of a and b is:')
+print(a*b)
+
+#divison
+print('divison of a by b is:')
+print(a/b)
+
