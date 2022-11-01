e0c7b554de05a488f0a269949d087ac76dbdb7db
anjunashs <117129615+anjunashs@users.noreply.github.com>
2022-10-31 21:58:04 -0400

    first committ

---
+++ hello-world.py
@@ -0,0 +1,20 @@
+#print hello message
+print('Hello World!')
+
+#assigning variables
+x = 7.8
+a = '2.5'
+z = 1.5
+b = '16.1'
+
+print('x + z is')
+print(x+z)                           #this should be a number'''
+print('')
+"""""" 
+print('a + b is')
+print(a+b)                           #this should be a string
+
+print('We good!')
+
+print('''goodbye EVERYONE!''')
+print("""See you next time""")
